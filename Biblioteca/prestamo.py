37df4c4dca6ba83ca61abc7e3d8a2772c9cc7fd8
Ignacio Huiza <ignaciohuiza@gmail.com>
2024-09-16 11:32:40 -0300

    clase1.4

--- Biblioteca/prestamo.py
+++ Biblioteca/prestamo.py
@@ -1,6 +1,7 @@
 class prestamo:
-    def __init__(self,isbn,nro_ident,fecha_prestamo,fecha_entrega,entregado):
+    def __init__(self,isbn,nro_ident,fecha_prestamo,fecha_entrega,fecha_devolucion):
         self.isbn = isbn
         self.nro_ident = nro_ident
         self.fecha_prestamo = fecha_prestamo
         self.fecha_entrega = fecha_entrega
+        self.fecha_devolucion = fecha_devolucion
