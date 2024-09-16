633d1526aafccec27099e51dd49a708a74d8c384
Ignacio Huiza <ignaciohuiza@gmail.com>
2024-09-16 10:29:22 -0300

    clases 1.3

--- Biblioteca/detalle_libro.py
+++ Biblioteca/detalle_libro.py
@@ -2,4 +2,4 @@ class detalle_libro:
     def __init__(self,categorias,nro_paginas,editorial):
         self.categorias = categorias
         self.nro_paginas = nro_paginas
-        self.editorial = nro_paginas
+        self.editorial = editorial
